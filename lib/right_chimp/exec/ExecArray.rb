cd3d9bf72c30591fe0c34c09527626d58fe764da
Nels Broberg <nels@rightscale.com>
2013-11-11 14:45:12 -0800

    acu137635 - Allow timeouts longer than 15 min on server arrays

--- lib/right_chimp/exec/ExecArray.rb
+++ lib/right_chimp/exec/ExecArray.rb
@@ -8,11 +8,16 @@ module Chimp
         audit_entry = []
         options = @inputs
 
+        if @timeout < 300
+          Log.error "timeout was less than 5 minutes! resetting to 5 minutes"
+          @timeout = 300
+        end
+
         audit_entry = @array.run_script_on_instances(@exec, @server['href'], options)
 
         if audit_entry
           audit_entry.each do |a|
-            a.wait_for_completed
+            a.wait_for_completed("no audit link available",@timeout)
           end
         else
           Log.warn "No audit entries returned for job_id=#{@job_id}"
