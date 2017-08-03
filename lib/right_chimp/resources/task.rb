f7e2931d595f9a4dc388d7ea3ad6755a9934ddb5
Marc Goujon <markitoxs@gmail.com>
2017-08-03 15:52:56 -0700

    OPS-4551 added the polling rate option

--- lib/right_chimp/resources/task.rb
+++ lib/right_chimp/resources/task.rb
@@ -9,6 +9,11 @@ module Chimp
     attr_writer :tasker
     attr_reader :tasker
 
+    attr_accessor :api_polling_rate
+    def initialize
+      self.api_polling_rate = ENV['API_POLLING_RATE'].to_i || 30
+    end
+
     def wait_for_state(desired_state, timeout = 900)
       while timeout > 0
         # Make compatible with RL10.
@@ -20,8 +25,9 @@ module Chimp
         if status.match('failed') || status.match('aborted')
           raise "FATAL error, #{status}\n\n Audit: #{friendly_url}\n "
         end
-        sleep 30
-        timeout -= 30
+        Log.debug "Polling again in #{self.api_polling_rate}"
+        sleep self.api_polling_rate
+        timeout -= self.api_polling_rate
       end
       raise "FATAL: Timeout waiting for Executable to complete.  State was #{status}" if timeout <= 0
     end
